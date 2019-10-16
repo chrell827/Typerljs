48ae315b5a259b5f249867f436a3acb4d42a6440
chrell827 <chrell827@gmail.com>
2019-10-15 21:12:58 -0400

    new file:   assets/css/style.css 	new file:   assets/game.js 	new file:   index.html

---
+++ assets/game.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+$("#ready-button").on("click",function(){
+    $("#intro-container").css({display: "none"});
+    $(".trivia").css({display: "block"});
+    
+})
+
+});
